304dbe16aabcda898cd37dfb8952f9991f123c6d
humanitiesNerd <catonano@gmail.com>
2013-06-29 23:47:28 +0200

    little polish to project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.namespace "0.2.1"]
-                 [org.clojure/core.incubator "0.1.3"]
+                 
                  [domina "1.0.1"]
                  [ch.qos.logback/logback-classic "1.0.6"]
                  [org.clojure/clojurescript "0.0-1450"]
